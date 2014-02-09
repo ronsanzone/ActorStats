2e0f61b33e2712f6ed12017b763ccace9976d197
Ron Sanzone <ronald.lee.sanzone@gmail.com>
2014-02-09 13:06:02 -0500

    Begin Customizing Fork of ngbp

--- karma/karma-unit.tpl.js
+++ karma/karma-unit.tpl.js
@@ -39,7 +39,7 @@ module.exports = function ( karma ) {
     /** 
      * Disable file watching by default.
      */
-    autoWatch: false,
+    autoWatch: true,
 
     /**
      * The list of browsers to launch to test on. This includes only "Firefox" by
@@ -55,7 +55,7 @@ module.exports = function ( karma ) {
      * the aesthetic advantage of not launching a browser every time you save.
      */
     browsers: [
-      'Firefox'
+      'PhantomJS'
     ]
   });
 };
